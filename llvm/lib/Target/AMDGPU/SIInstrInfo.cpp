a006b8c2bce38ddbaa4f73d98dbdb0f3ca58f3ea
pvanhout <pierre.vanhoutryve@amd.com>
2026-03-30 11:55:36 +0200

    [AMDGPU] Make globally-addressable-scratch opt-in
    This feature is meant to be opt-in for more advanced users, not default-enabled.
    It may reduce performance otherwise as we can't assume private AS is thread-local
    when it is enabled.
    
    - Add `HasGloballyAddressableScratchSupport` feature to check if a target's scratch
      addressing is changed due to support for globally addressable scratch.
    - Use `EnableGloballyAddressableScratch` to check whether the user opted into
      globally addressable scratch. This affects whether to lower scratch atomics as flat,
      and in the future will affect whether NV=1 can be set on scratch accesses.

--- llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
@@ -10930,7 +10930,7 @@ SIInstrInfo::getGenericValueUniformity(const MachineInstr &MI) const {
     unsigned SrcAS = SrcTy.getAddressSpace();
     return SrcAS == AMDGPUAS::PRIVATE_ADDRESS &&
                    DstAS == AMDGPUAS::FLAT_ADDRESS &&
-                   ST.hasGloballyAddressableScratch()
+                   ST.hasGloballyAddressableScratchSupport()
                ? ValueUniformity::NeverUniform
                : ValueUniformity::Default;
   };
