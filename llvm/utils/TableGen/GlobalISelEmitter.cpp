9f3f814910519c4c751449bdfdf305cb2c990adf
pvanhout <pierre.vanhoutryve@amd.com>
2026-06-01 11:32:34 +0200

    [NFC][GlobalISel] Refactor ownership of InstructionMatchers
    - Clarify that the array of InstructionMatchers in the RuleMatcher are for the roots only.
    - Let RuleMatcher own all of the InstructionMatcher used for/by predicates.
      They are all kept in an array in which the index of the InstructionMatcher is equal to its
      InsnID, which eliminates some redundant tracking.
    - Remove duplicate tracking of InsnID; InstructionMatcher does it on its own already.

--- llvm/utils/TableGen/GlobalISelEmitter.cpp
+++ llvm/utils/TableGen/GlobalISelEmitter.cpp
@@ -2215,7 +2215,7 @@ Expected<RuleMatcher> GlobalISelEmitter::runOnPattern(const PatternToMatch &P) {
       M.addAction<ConstrainOperandToRegClassAction>(0, 0, RC);
 
       // Erase the root.
-      unsigned RootInsnID = M.getInsnVarID(InsnMatcher);
+      unsigned RootInsnID = InsnMatcher.getInsnVarID();
       M.addAction<EraseInstAction>(RootInsnID);
 
       // We're done with this pattern!  It's eligible for GISel emission; return
@@ -2298,7 +2298,7 @@ Expected<RuleMatcher> GlobalISelEmitter::runOnPattern(const PatternToMatch &P) {
     return std::move(Error);
 
   // Erase the root.
-  unsigned RootInsnID = M.getInsnVarID(InsnMatcher);
+  unsigned RootInsnID = InsnMatcher.getInsnVarID();
   M.addAction<EraseInstAction>(RootInsnID);
 
   // We're done with this pattern!  It's eligible for GISel emission; return it.
